30e88f2a45026874eef606fb849dbc58edb55ebf
Alberto M. Merinero Santos <alberto_merinero@iecisa.com>
2017-11-02 18:15:12 +0100

    logfile2cloud Working

--- GCP/logfile2gcloud-datastore.py
+++ GCP/logfile2gcloud-datastore.py
@@ -1,6 +1,7 @@
 from pyparsing import Word, alphas, alphanums, nums, Combine, Optional, Suppress, Regex
 from google.cloud import datastore
 import datetime
+import pytz
 
 def log2cloud (client,type,line):
 
@@ -13,17 +14,27 @@ def log2cloud (client,type,line):
     Horas = ints
     Mins = ints
     Segs = ints
-    timestamp = month + day + Horas + Suppress(":") + Mins + Suppress(":") + Segs
+    #timestamp = month + day + Horas + Suppress(":") + Mins + Suppress(":") + Segs
+    hour  = Combine(ints + ":" + ints + ":" + ints)
+    timestamp = month + day + hour
     hostname = Word(alphas + nums + "_" + "-" + ".")
     appname = Word(alphas + "/" + "-" + "_" + ".") + Optional(Suppress("[") + ints + Suppress("]")) + Suppress(":")
     message = Regex(".*")
     logline = timestamp + hostname + appname + message
-    mess_prueba = "Oct 26 08:30:01 MX3750006dc0458 systemd[1]: Started Session 653 of user mfe."
-    #campos = logline.parseString(mess_prueba)
+    mess_prueba = "Oct 28 08:30:01 MX3750006dc0458 systemd[1]: Started Session 653 of user mfe."
+    datefmt = '%Y%b%d%H:%M:%S'
+    Year='2017'
+    timezone=pytz.timezone("CET")
     campos = logline.parseString(line)
-    for i in range(len(campos)):
-        print ("Campo[",i,"]=",campos[i])
-
+    # for i in range(len(campos)):
+    #     print ("Campo[",i,"]=",campos[i])
+    #
+    # print (Year+campos[0]+campos[1]+campos[2])
+    fecha = datetime.datetime.strptime(Year+campos[0]+campos[1]+campos[2],datefmt)
+    #fecha.tzinfo=CET
+    # print (fecha)
+    fechatz = timezone.localize(fecha)
+    # print (fechatz)
     # Despues lo inseretamos en la bbdd de la nube
 
     key = client.key(type)
@@ -31,10 +42,12 @@ def log2cloud (client,type,line):
         key, exclude_from_indexes=['Mensaje'])
 
     logline_db.update({
-        'Fecha': campos[0]+campos[1]+campos[2],
-        'Hostname': campos[3],
-        'Mensaje': campos[4]+campos[5]+campos[6]
-    })
+         'Fecha': fechatz,
+         'Hostname': unicode(campos[3]),
+         'Proceso': unicode(campos[4]),
+         'PID': int(campos[5]),
+         'Mensaje': unicode(campos[6])
+     })
     client.put(logline_db)
     return logline_db.key
 
@@ -42,6 +55,9 @@ def log2cloud (client,type,line):
 #print (mess_prueba, "->", campos)
 #for i in range(len(campos)):
 #    print ("Campo[",i,"]=",campos[i])
-Linea ="Oct 26 08:30:01 MX3750006dc0458 systemd[1]: Started Session 653 of user mfe."
+Linea ="Oct 30 08:30:01 MX3750006dc0458 systemd[1]: Started Session 653 of user mfe."
 client = datastore.Client('unix-logsgraphs')
-log2cloud (client,'LogMensaje',Linea)
+with open('./sample.log') as syslogFile:
+  for line in syslogFile:
+      print line
+      log2cloud (client,'LogMensaje',line)
