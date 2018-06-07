840beedc26fb758eb14199211516d3563224a7f5
Alberto Merinero @ MX3750006dc0458 <alberto_merinero@iecisa.com>
2018-06-07 09:48:45 +0200

    actualizamos...

--- AWS/voicemod_awslogs_conf.sh
+++ AWS/voicemod_awslogs_conf.sh
@@ -1,19 +1,26 @@
 [general]
 state_file = /var/awslogs/state/agent-state
 
+[/var/log/messages]
+datetime_format = %b %d %H:%M:%S
+file = /var/log/messages
+buffer_duration = 5000
+log_stream_name = {instance_id}
+initial_position = start_of_file
+log_group_name = centos
 
 [httpd-access_log]
-datetime_format = %Y-%m-%d %H:%M:%S
+datetime_format = %b %d %H:%M:%S
 file = /var/log/httpd/access_log
 buffer_duration = 5000
 log_stream_name = httpd-access_log
 initial_position = start_of_file
-log_group_name = ecs
+log_group_name = centos
 
 [httpd-error_log]
-datetime_format = %Y-%m-%d %H:%M:%S
+datetime_format = %b %d %H:%M:%S
 file = /var/log/httpd/error_log
 buffer_duration = 5000
 log_stream_name = httpd-error_log
 initial_position = start_of_file
-log_group_name = ecs
+log_group_name = centos
--- AWS/voicemod_script.sh
+++ AWS/voicemod_script.sh
@@ -13,3 +13,23 @@ service httpd start
 service awslogs start
 chkconfig httpd on
 chkconfig awslogs on 
+
+Sobre CentOS 7
+
+#!/bin/bash
+yum install php stress awscli -y
+yum update -y
+mv /etc/localtime /etc/localtime.orig
+ln -s /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
+pip install --upgrade s3transfer
+aws s3 cp s3://merinerowebbucket001/phpweb /var/www/html/ --recursive
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+service httpd start
+service awslogs start
+chkconfig httpd on
+chkconfig awslogs on 
